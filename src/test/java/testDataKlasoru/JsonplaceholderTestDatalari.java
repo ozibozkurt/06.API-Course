8d5eca76afabb2d6f8f9e8e96fe36ee1c5bb8eb7
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-06-13 14:05:53 +0300

    C19 baslandi

--- src/test/java/testDataKlasoru/JsonplaceholderTestDatalari.java
+++ src/test/java/testDataKlasoru/JsonplaceholderTestDatalari.java
@@ -5,6 +5,8 @@ import org.json.JSONObject;
 public class JsonplaceholderTestDatalari {
 
    public int basariliStatusCode = 200;
+   public String contentType="application/json; charset=utf-8";
+   public String headerConnection="keep-alive";
 
 
     public JSONObject getRequestExpectedBody() {
@@ -16,4 +18,28 @@ public class JsonplaceholderTestDatalari {
 
         return expectedDataJson;
     }
+
+    public JSONObject putRequestBody() {
+        JSONObject RequestBody=new JSONObject();
+        RequestBody.put("userId",10);
+        RequestBody.put("id",70);
+        RequestBody.put("title","Ahmet");
+        RequestBody.put("body","Merhaba");
+
+        return RequestBody;
+    }
+
+    public JSONObject putRequestExpectedBody() {
+        JSONObject putRequestExpectedBodyJson=new JSONObject();
+        putRequestExpectedBodyJson.put("userId",10);
+        putRequestExpectedBodyJson.put("id",70);
+        putRequestExpectedBodyJson.put("title","Ahmet");
+        putRequestExpectedBodyJson.put("body","Merhaba");
+
+        return putRequestExpectedBodyJson;
+    }
+
+
+
+
 }
